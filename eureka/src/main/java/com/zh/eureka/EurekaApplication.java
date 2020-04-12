773f5db9260ec65b483be3fc2159b47fb7933b6b
zh <164675423@qq.com>
2020-04-12 23:07:25 +0800

    eureka migrate

---
+++ eureka/src/main/java/com/zh/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.zh.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心入口
+ *
+ * @author zh
+ * @date 2020/2/16
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+
+}
---
+++ eureka/src/test/java/com/zh/eureka/EurekaApplicationTests.java
@@ -0,0 +1,8 @@
+package com.zh.eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+}
